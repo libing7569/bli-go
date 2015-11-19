5067b2b503f3644f9fb5040f4a4b73042404efb2
EC2 Default User <ec2-user@ip-172-31-23-80.cn-north-1.compute.internal>
2015-11-19 09:09:41 +0000

    add test

---
+++ pool_test.go
@@ -0,0 +1,30 @@
+package pool
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestGPool(t *testing.T) {
+	t.Log("Hello")
+
+	pool := NewGPool(2)
+	pool.Start()
+	pool.GetResult()
+	//pool.Heatbeat()
+
+	for i := 0; i < 1000; i++ {
+		pool.AddTask(func(args ...interface{}) interface{} {
+			fmt.Println("Add TaskId")
+			//fmt.Println(reflect.TypeOf(args))
+			fmt.Printf("%#v\n", args)
+			return args
+		}, i)
+	}
+	pool.Stop()
+	time.Sleep(time.Second)
+	if pool.TotalTask != 1000 {
+		t.Error("Task Lost!")
+	}
+}
